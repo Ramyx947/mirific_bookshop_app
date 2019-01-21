2c72595b256117213fc6bcff332f3982355d4001
Ramona <r1mari@yahoo.com>
2019-01-21 10:50:12 +0000

    added validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,10 @@
 class Book < ApplicationRecord
+    validates :title, :description, :image_url, presence: true
+    validates :title, uniqueness: true
+    validates :price, numericality: {greater_than_or_equal_to: 0.01}
+    validates :image_url, allow_blank: true, format: 
+    { 
+        with: %r{.(gif|jpg|png)\Z}i, 
+        message: 'must be a URL for GIF, JPG or PNG image.' 
+    }
 end
